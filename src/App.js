27325429b739835e8b804f883aaa4d2d1e3a0575
annu <annupawadia@gmail.com>
2024-09-28 15:33:24 +0530

    making different components in one app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,8 @@
 import React from 'react'
-import FileUploadComponent from './components/FileUploadComponent'
-import FirebaseAuth from './components/FirebaseAuth'
-import ImageUploadAndManipulate from './components/ImageUploadAndManipulate'
 
 const App = () => {
   return (
-    <div>
-
-
-      <FileUploadComponent/>
-      <FirebaseAuth/>
-      <ImageUploadAndManipulate/>
-    </div>
+    <div>App</div>
   )
 }
 
