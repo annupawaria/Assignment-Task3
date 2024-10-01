f6442083618ae60694dbf0ce5f64cc5f3ef42050
annu <annupawadia@gmail.com>
2024-10-01 16:42:10 +0530

    applying pagination

--- src/components/Validate1.js
+++ src/components/Validate1.js
@@ -3,9 +3,17 @@ import React, { useEffect, useState } from 'react'
 
 const Validate1 = () => {
   const[tableData,setTableData]=useState("")
+  const[currentPage,setCurrentPage]=useState(1)
+  const[rowsPerPage,setRowsPerPage]=useState(10)
+  const indexOfLastItem=currentPage*rowsPerPage;
+  const indexOfFirstItem=indexOfLastItem-rowsPerPage;
+  const currentItems=tableData?.users?.slice(indexOfFirstItem,indexOfLastItem)
+  const totalPages=Math.ceil(tableData?.total/rowsPerPage)
+
   useEffect(()=>{
     axios.get("https://dummyjson.com/users?limit=0").then((response)=>{
 console.log(response)
+setTableData(response?.data)
     })
   },[])
   return (
